bfb7355792b3e9e81696e732e2cd719ded4d5a33
pasha28198 <pavlognidin@gmail.com>
2021-06-13 19:04:21 +0300

    add fix

--- src/request/request.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const token = localStorage.getItem('accessToken')
-const instance  = axios.create({
-  baseURL: 'https://pizzavovaapi.herokuapp.com/',
-  // headers: {'Authorization': `${token}`}
-});
-
-export default instance;
