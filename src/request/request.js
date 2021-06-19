2b1433f6e650e5c33c4805c8e17ac352e92dfeea
pasha28198 <pavlognidin@gmail.com>
2021-06-19 17:38:49 +0300

    add fix

--- src/request/request.js
+++ src/request/request.js
@@ -3,17 +3,8 @@ const token = localStorage.getItem('accessToken')
 
 axios.defaults.headers['Authorization'] = `Bearer ${token}`
 
-const headers = new Headers({
-  'Accept-Version': 1,
-  'Accept': 'application/json',
-  'Access-Control-Allow-Origin': '*',
-  'Content-Type': 'application/json; charset=utf-8',
-  'Authorization': `Bearer ${token}`
-})
-
 const instance  = axios.create({
   baseURL: 'https://whiteboxpizza.com.ua/api/',
-  headers
 });
 
 export default instance;
