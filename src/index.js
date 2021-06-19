d74f5dbdcbe001996f3d974ad1caabdf0baae375
pasha28198 <pavlognidin@gmail.com>
2021-06-19 17:09:08 +0300

    update auth part

--- src/index.js
+++ src/index.js
@@ -3,17 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
-// import {Redirect} from 'react-router';
 import {ScrollContext} from 'react-router-scroll-4';
 import * as serviceWorker from './serviceWorker';
 
-// ** Import custom components for redux**
 import {Provider} from 'react-redux';
 import store from './store/index';
 import App from "./components/app";
 
-// Import custom Components
-import Dashboard from './components/dashboard/dashboard';
 import Coupons from './components/coupons/coupons';
 import Ingredients from './components/ingredients/ingredients';
 import Products from './components/products/products';
@@ -21,13 +17,11 @@ import Orders from './components/orders/orders';
 import ProductsId from './components/products_id/products';
 import Login from './components/login';
 import Logout from './components/logout';
-import Error404 from './components/error404';
 import changePassword from './components/changePassword';
 
 import PrivateRoute from './PrivateRoute';
 
 function withLayout(WrappedComponent) {
-    // ...and returns another component...
     return class extends React.Component {
         render() {
             return <App>
--- src/request/request.js
+++ src/request/request.js
@@ -1,9 +1,13 @@
 import axios from 'axios';
-
 const token = localStorage.getItem('accessToken')
+
+const headers = new Headers({
+  'Authorization': `Bearer ${token}`
+})
+
 const instance  = axios.create({
   baseURL: 'https://whiteboxpizza.com.ua/api/',
-  headers: {'Authorization': `${token}`}
+  headers
 });
 
 export default instance;
